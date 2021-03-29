d632cef9a347ad4de9496093d40d1b7a558ad255
ThomasVDP <T1@terinfo.net>
2021-03-29 16:47:58 +0200

    Update BotMemory.java

--- src/main/java/com/github/grizzlt/botbaselib/core/memory/BotMemory.java
+++ src/main/java/com/github/grizzlt/botbaselib/core/memory/BotMemory.java
@@ -5,7 +5,6 @@ import net.openhft.chronicle.bytes.BytesMarshallable;
 import net.openhft.chronicle.map.ChronicleMap;
 
 import java.io.IOException;
-import java.util.function.Supplier;
 
 public class BotMemory
 {
@@ -64,14 +63,20 @@ public class BotMemory
      * Gets the current value associated with the given key or computes one if none was supported yet
      *
      * @param key
-     * @param defaultFactory
+     * @param defaultValue
      * @param <T> dataType (must be {@link BytesMarshallable}, e.g. Chronicle value interface)
      * @return the current {@link Entry} associated with the given key
      */
     @SuppressWarnings("unchecked")
-    public <T extends BytesMarshallable> Entry<T> getOrDefault(String key, Supplier<T> defaultFactory)
+    public <T extends BytesMarshallable> Entry<T> getOrDefault(String key, Entry<T> defaultValue)
     {
-        return (Entry<T>)this.memoryMap.computeIfAbsent(key, ignore -> Entry.of(defaultFactory.get()));
+        Entry<T> entry = (Entry<T>)this.memoryMap.getUsing(key, defaultValue);
+        if (entry == null)
+        {
+            this.memoryMap.put(key, defaultValue);
+            return defaultValue;
+        }
+        return entry;
     }
 
     /**
